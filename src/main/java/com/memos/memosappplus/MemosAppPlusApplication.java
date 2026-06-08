bc96eee9e1d45f0259de1213904f5a22336635d0
周睿 <1875858608@qq.com>
2026-06-08 13:34:49 +0800

    first commit

---
+++ src/main/java/com/memos/memosappplus/MemosAppPlusApplication.java
@@ -0,0 +1,13 @@
+package com.memos.memosappplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemosAppPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemosAppPlusApplication.class, args);
+    }
+
+}
